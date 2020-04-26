2b3c67a8c7db3aadbf8ab396ce6f592e4798515d
pjmantoss <joelptoss@gmail.com>
2020-04-26 20:21:54 +0100

    UPDATE App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 const API_KEY = "16244904-b76f429eabdbad43dc83fbdce";
 
+const url =  `https://pixabay.com/api/?key=${API_KEY}&q=yellow+flowers&image_type=photo`
+
 function App() {
   return (
     <div>

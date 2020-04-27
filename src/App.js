49a167c79238441cf96394c2175cfa3d19a58560
pjmantoss <joelptoss@gmail.com>
2020-04-27 18:04:53 +0100

    UPDATE App.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,19 @@ const API_KEY = "16244904-b76f429eabdbad43dc83fbdce";
 const url =  `https://pixabay.com/api/?key=${API_KEY}&q=yellow+flowers&image_type=photo`;
 
 export class App extends Component {
+
+  handleGetRequest = () => {
+    console.log('Working!!')
+  }
+
+  componentDidMount(){
+    this.handleGetRequest();
+  }
+
   render() {
     return (
       <div>
-        
+        <div>App Component</div>
       </div>
     )
   }

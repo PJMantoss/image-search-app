421f1720bac7ee88885b8701519c55f49e3c7c3d
pjmantoss <joelptoss@gmail.com>
2020-04-27 17:59:39 +0100

    UPDATE App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 const API_KEY = "16244904-b76f429eabdbad43dc83fbdce";
 
-const url =  `https://pixabay.com/api/?key=${API_KEY}&q=yellow+flowers&image_type=photo`
+const url =  `https://pixabay.com/api/?key=${API_KEY}&q=yellow+flowers&image_type=photo`;
 
-function App() {
-  return (
-    <div>
-      <span role="img" aria-label="emoji">What up 😎</span>
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App

ff2e15a68ab3335f5ed08246c469e516d296a801
pjmantoss <joelptoss@gmail.com>
2020-04-27 19:45:57 +0100

    update ImageList

--- src/ImageList/ImageList.js
+++ src/ImageList/ImageList.js
@@ -2,15 +2,24 @@ import React from 'react'
 
 export const ImageList = (props) => {
     return (
-        <div>
-            { props.images.map(({ id, largeImageURL, tags }) => {
-          return (
-              <div key={id}>
-                  <img src={largeImageURL} alt={tags} />
-                  <button>Search</button>
-              </div>
-             )
-        })}
+        <div className="container">
+
+            <div className="row">
+                { props.images.map(({ id, largeImageURL, tags }) => {
+                    return (
+                        <div key={id} className="col-md-4">
+
+                            <div className="imageList__container">
+                                <img src={largeImageURL} alt={tags} className="imageList__image" />
+                            </div>
+
+                            <button>Search</button>
+                        </div>
+                        )
+                    })
+                }
+            </div>
+
         </div>
     )
 }

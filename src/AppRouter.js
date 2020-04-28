6c1ea56680a343d1786bb165521ee71ae0bde0a6
pjmantoss <joelptoss@gmail.com>
2020-04-28 17:42:57 +0100

    update AppRouter

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { HashRouter, Switch, Route} from 'react-router-dom';
+import App from './App';
+import {ImageView} from './ImageView/ImageView';
 
 export const AppRouter = () => {
     return (
         <HashRouter>
             <Switch>
-                
+                <Route path="/" component={App} exact />
+                <Route path="/image/:id" component={ImageView} />
             </Switch>
         </HashRouter>
     )

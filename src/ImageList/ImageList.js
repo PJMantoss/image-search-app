7aec311dbaa89268a1386e8fd94ebebcb9fa7000
pjmantoss <joelptoss@gmail.com>
2020-04-28 17:51:36 +0100

    update ImageList

--- src/ImageList/ImageList.js
+++ src/ImageList/ImageList.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './ImageList.css';
 
-export const ImageList = (props) => {
+export const ImageList = ({images}) => {
     return (
         <div className="container">
 
             <div className="imageList">
-                { props.images.map(({ id, largeImageURL, tags }) => {
+                { images.map(({ id, largeImageURL, tags }) => {
                     return (
                         <div key={id} className="imageList__container">
                                 <img src={largeImageURL} alt={tags} className="imageList__image" />
                                 <div className="image__details">
-                                    <button>View</button>
+                                    <Link>
+                                        <button>View</button>
+                                    </Link>
                                 </div>
                         </div>
                         )

dc3163ea097a1cf5a5f1c21867004cd0195b6219
pjmantoss <joelptoss@gmail.com>
2020-04-28 16:25:08 +0100

    update styling for ImageList

--- src/ImageList/ImageList.js
+++ src/ImageList/ImageList.js
@@ -9,13 +9,10 @@ export const ImageList = (props) => {
                 { props.images.map(({ id, largeImageURL, tags }) => {
                     return (
                         <div key={id} className="imageList__container">
-                            
                                 <img src={largeImageURL} alt={tags} className="imageList__image" />
                                 <div className="image__details">
                                     <button>Search</button>
                                 </div>
-                            
-                            
                         </div>
                         )
                     })
